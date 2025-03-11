ebe3104d1a526dfc0ec2150fe29b1d2cca66bba2
Hanák Renáta <hanakrenata@ktch.hu>
2025-03-11 09:40:01 +0100

    Final

--- src/App.java
+++ src/App.java
@@ -12,6 +12,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        System.out.println("Készítő: Hanák Renáta");
+        System.out.println("Osztály: Szoft 1/n");
+        System.out.println("Dátum: 2025-03-11 \n");
+
         Solution sol = new Solution();
 
         sol.findOlderWorker();
